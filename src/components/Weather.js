dff862e03bf3ebdac44c4b6625fe66a79acb8bba
jabbarlee <amiljabbarlee@gmail.com>
2024-01-23 16:26:56 -0600

    city options added

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -34,17 +34,33 @@ function Weather() {
         }, 1000);
 
         return () => clearInterval(interval)
-    }, [weather])
+    }, [weather, city])
+
+    const cities = [
+        'New York', 'Baku', 'Cairo',
+        'Moscow', 'Rome', 'Paris', 'Houston'
+    ]
   return (
-    <div className='weather-widget'>
-        <div id='temp-side'>
-            <img src={icon}/>
-            <p id='temp-label'>{weather} °F</p>
-            <p>{city}</p>
+    <div id='wrapper'>
+        <div className='weather-widget'>
+            <div id='temp-side'>
+                <img src={icon}/>
+                <p id='temp-label'>{weather} °F</p>
+                <p>{city}</p>
+            </div>
+            <div id='info-side'>
+                <p className='info-label'>Wind: {wind} mph</p>
+                <p className='info-label'>UV Index: {uv}</p>
+            </div>
         </div>
-        <div id='info-side'>
-            <p className='info-label'>Wind: {wind} mph</p>
-            <p className='info-label'>UV Index: {uv}</p>
+        <div className='option'>
+            <select id='city-option' value={city} onChange={(e) => setCity(e.target.value)}>
+                {cities.map((city) => {
+                    return(
+                        <option value={city}>{city}</option>
+                    )
+                })}
+            </select>
         </div>
     </div>
   )
